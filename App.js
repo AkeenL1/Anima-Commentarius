7de165623872e9834c5256d74d2738a4c63fa918
akeenl-sp <akeen.lewis@smartrent.com>
2023-08-12 19:13:51 -0400

    change

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>AKeen</Text>
+      <Text>Akeen</Text>
       <StatusBar style="auto" />
     </View>
   );
