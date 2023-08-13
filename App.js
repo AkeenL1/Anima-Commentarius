733a9597520eda42df29216e1bac074a50ea5016
akeenl-sp <akeen.lewis@smartrent.com>
2023-08-13 16:41:22 -0400

    work

--- App.js
+++ App.js
@@ -1,20 +1,45 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {Alert, Button, Dimensions, Image, Text, View} from 'react-native';
+import styles from "./assets/styles/app.styles";
+import picture_logo from "./assets/images/anima_commentarius_logo_circle.png"
+import text_logo from "./assets/images/Untitled(1).png"
+const { width, height } = Dimensions.get('window');
+import { SafeAreaView } from 'react-native';
+import { useFonts } from 'expo-font';
 
 export default function App() {
-  return (
+    const [loaded] = useFonts({
+        Roman: require('./assets/fonts/Romana-Regular.otf'),
+    });
+
+    if (!loaded) {
+        return null;
+    }
+    return (
     <View style={styles.container}>
-      <Text>Akeen</Text>
-      <StatusBar style="auto" />
+        <Image
+            source={picture_logo}
+            style={{
+                width: width,
+                height: height * 0.3,
+                top: 100
+            }}
+        />
+        <Image
+            source={text_logo}
+            style={{
+                width: width,
+                height: height* .09,
+                bottom: 120
+            }}
+        />
+        <View style={styles.loginButton}>
+            <Button
+                title="Login"
+                onPress={() => Alert.alert('Cannot press this one')}
+            />
+        </View>
+        <StatusBar />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ assets/styles/app.styles.js
@@ -0,0 +1,15 @@
+import {StyleSheet} from "react-native";
+
+const styles = StyleSheet.create({
+    container: {
+        display: "flex",
+        flex: 1,
+        justifyContent: "space-between",
+        backgroundColor: '#c2af88',
+    },
+    loginButton: {
+        bottom: 200,
+    }
+});
+
+export default styles
