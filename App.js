30482daf480c4064b9f343658247f37a723a58cf
akeenl-sp <akeen.lewis@smartrent.com>
2023-08-13 23:41:42 -0400

    more work

--- App.js
+++ App.js
@@ -1,8 +1,9 @@
 import { StatusBar } from 'expo-status-bar';
-import {Alert, Button, Dimensions, Image, Text, View} from 'react-native';
+import {Alert, Button, Dimensions, Image, Text, View, Pressable} from 'react-native';
 import styles from "./assets/styles/app.styles";
-import picture_logo from "./assets/images/anima_commentarius_logo_circle.png"
-import text_logo from "./assets/images/Untitled(1).png"
+import picture_logo from "./assets/images/anima_commentarius_logo_circle_v2(1).png"
+import text_logo from "./assets/images/ac_text_logo_2.png"
+import { LinearGradient } from 'expo-linear-gradient';
 const { width, height } = Dimensions.get('window');
 import { SafeAreaView } from 'react-native';
 import { useFonts } from 'expo-font';
@@ -17,29 +18,33 @@ export default function App() {
     }
     return (
     <View style={styles.container}>
-        <Image
-            source={picture_logo}
-            style={{
-                width: width,
-                height: height * 0.3,
-                top: 100
-            }}
-        />
-        <Image
-            source={text_logo}
-            style={{
-                width: width,
-                height: height* .09,
-                bottom: 120
-            }}
-        />
-        <View style={styles.loginButton}>
-            <Button
-                title="Login"
-                onPress={() => Alert.alert('Cannot press this one')}
+        <LinearGradient
+            colors={['rgb(0,20,86)',"rgb(67,12,77)", "black"]}
+            style={styles.background}
+        >
+            <Image
+                source={picture_logo}
+                style={{
+                    width: width,
+                    height: height * 0.4,
+                    top: 100
+                }}
             />
-        </View>
-        <StatusBar />
+            <Image
+                source={text_logo}
+                style= {{
+                    width: width,
+                    resizeMode: 'contain',
+                    bottom: 120
+                }}
+            />
+            <View style={styles.loginView}>
+                <Pressable style={styles.loginButton}>
+                    <Text style={styles.text}>Login</Text>
+                </Pressable>
+            </View>
+            <StatusBar />
+        </LinearGradient>
     </View>
   );
 }
--- assets/styles/app.styles.js
+++ assets/styles/app.styles.js
@@ -1,14 +1,34 @@
 import {StyleSheet} from "react-native";
+import {useFonts} from "expo-font";
 
 const styles = StyleSheet.create({
     container: {
+      height: "100%"
+    },
+    background: {
         display: "flex",
-        flex: 1,
         justifyContent: "space-between",
-        backgroundColor: '#c2af88',
+        height: "100%"
+    },
+    loginView: {
+        bottom: 225,
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center"
     },
     loginButton: {
-        bottom: 200,
+        borderWidth: 2,
+        borderColor: "#a07b89be344",
+        borderRadius: 20,
+        width: "75%",
+        backgroundColor: 'rgba(159,158,158,0.5)'
+    },
+    text: {
+        padding: 5,
+        fontSize: 33,
+        color: "white",
+        textAlign: "center",
+        fontFamily: "Roman"
     }
 });
 
